40e924ab1de1ec1053b02fd2c6f84e485a3c4e78
Jorge Arias <94802496+joariasdev@users.noreply.github.com>
2025-07-02 18:39:34 -0400

    Add main loop and switch skeleton

--- Tareas/TareaTres/Program.cs
+++ Tareas/TareaTres/Program.cs
@@ -10,3 +10,29 @@ Dictionary<int, string> telephones = new Dictionary<int, string>();
 Dictionary<int, string> emails = new Dictionary<int, string>();
 Dictionary<int, int> ages = new Dictionary<int, int>();
 Dictionary<int, bool> bestFriends = new Dictionary<int, bool>();
+
+while (isRunning)
+{
+    Console.WriteLine(@"1. Agregar Contacto     2. Ver Contactos    3. Buscar Contactos     4. Modificar Contacto   5. Eliminar Contacto    6. Salir");
+    Console.WriteLine("Digite el número de la opción deseada");
+
+    int chosenOption = Convert.ToInt32(Console.ReadLine());
+
+    switch (chosenOption)
+    {
+        case 1: // Agregar Contacto
+            break;
+        case 2: // Ver Contactos
+            break;
+        case 3: // Buscar Contactos
+            break;
+        case 4: // Modificar Contacto
+            break;
+        case 5: // Eliminar Contacto
+            break;
+        case 6: // Salir
+            break;
+        default:
+            break;
+    }
+}
