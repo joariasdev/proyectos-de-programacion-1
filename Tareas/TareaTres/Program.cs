41cd07f5e6856520bd0f0be1a5884dda0b0abba4
Jorge Arias <94802496+joariasdev@users.noreply.github.com>
2025-07-02 18:32:36 -0400

    Add data repositories

--- Tareas/TareaTres/Program.cs
+++ Tareas/TareaTres/Program.cs
@@ -1,2 +1,12 @@
 ﻿Console.WriteLine("Bienvenido a mi lista de Contactos");
 
+bool isRunning = true;
+
+List<int> ids = new List<int>();
+Dictionary<int, string> names = new Dictionary<int, string>();
+Dictionary<int, string> lastnames = new Dictionary<int, string>();
+Dictionary<int, string> addresses = new Dictionary<int, string>();
+Dictionary<int, string> telephones = new Dictionary<int, string>();
+Dictionary<int, string> emails = new Dictionary<int, string>();
+Dictionary<int, int> ages = new Dictionary<int, int>();
+Dictionary<int, bool> bestFriends = new Dictionary<int, bool>();
