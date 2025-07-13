7c5329c94c046b2605c90b0051c059643c7686fe
Jorge Arias <94802496+joariasdev@users.noreply.github.com>
2025-07-13 18:51:01 -0400

    Add contact class

---
+++ Tareas/TareaCuatro/Contact.cs
@@ -0,0 +1,26 @@
+﻿namespace TareaCuatro
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public bool IsBestFriend { get; set; }
+
+        public Contact(int id, string firstName, string lastName, string address, string phone, string email, int age, bool isBestFriend)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+            Phone = phone;
+            Email = email;
+            Age = age;
+            IsBestFriend = isBestFriend;
+        }
+    }
+}
